01b2d9fd78aec8f8ff524836545d289280f02154
Tales Pinto <tales.pinto@hotmail.com>
2023-06-27 16:03:09 +1000

    first commit

---
+++ src/test/java/test/SeleniumTest.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    public void SeleniumTest(){
+        WebDriver driver = new ChromeDriver();
+
+    }
+}
