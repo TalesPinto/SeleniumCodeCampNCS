89c729d538ffc9be63e206b260d836543e2e1310
Tales Pinto <tales.pinto@hotmail.com>
2023-06-27 16:57:26 +1000

    adds title validation and button click test

--- src/test/java/test/SeleniumTest.java
+++ src/test/java/test/SeleniumTest.java
@@ -1,12 +1,46 @@
 package test;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SeleniumTest {
 
+    @Test
     public void SeleniumTest(){
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
 
+        driver.get("https://d18u5zoaatmpxx.cloudfront.net/");
+        driver.findElement(By.id("forename")).sendKeys("Tales");
+        driver.findElement(By.id("submit")).click();
+
+    }
+
+    @Test
+    public void TitleValidation() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://d18u5zoaatmpxx.cloudfront.net/");
+        String title = driver.findElement(By.xpath("//h1[@class=\"display-1 mb-3\"]")).getText();
+
+        Assertions.assertEquals(title, "Web Playground");
+
+    }
+
+    @Test
+    public void ButtonClickingTest() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://d18u5zoaatmpxx.cloudfront.net/");
+        driver.findElement((By.xpath("//div/a[@role=\"button\"]"))).click();
     }
 }
